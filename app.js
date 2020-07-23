f00294081eace50409c3382412a7b397cb8e8b8a
Tyler <43707450+changty97@users.noreply.github.com>
2020-07-23 09:28:45 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module('myApp', ['ngRoute']);
+ 
+app.config(function($routeProvider) {
+$routeProvider
+ 
+.when('/', {
+templateUrl : 'pages/first.html',
+controller : 'FirstController'
+})
+ 
+.when('/login', {
+templateUrl : 'pages/second.html',
+controller : 'SecondController'
+})
+ 
+.when('/about', {
+templateUrl : 'pages/third.html',
+controller : 'ThirdController'
+})
+ 
+.otherwise({redirectTo: '/'});
+});
+
+app.controller('FirstController', function($scope) {
+    $scope.message = 'Hello from FirstController';
+});
+     
+app.controller('SecondController', function($scope) {
+    $scope.message = 'Hello from SecondController';
+});
+     
+app.controller('ThirdController', function($scope) {
+    $scope.message = 'Hello from ThirdController';
+});
