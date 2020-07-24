77bd708ff2c24bd6402c481a92c5efa1a4258e81
Tyler <43707450+changty97@users.noreply.github.com>
2020-07-23 21:34:04 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -21,8 +21,17 @@ controller : 'ThirdController'
 .otherwise({redirectTo: '/'});
 });
 
+app.controller('MyCtrl',['$scope',function($scope)
+ {
+    $scope.firstPage = function() {
+        // if(document.getElementById('navbar')) {
+            console.log('hello');
+        // }
+    }
+}]);
+
 app.controller('FirstController', function($scope) {
-    $scope.message = 'Hello from FirstController';
+    $scope.message = "Welcome to Tyler's Cloud Service";
 });
      
 app.controller('SecondController', function($scope) {
@@ -31,4 +40,14 @@ app.controller('SecondController', function($scope) {
      
 app.controller('ThirdController', function($scope) {
     $scope.message = 'Hello from ThirdController';
+});
+
+app.controller('myController', function ($scope) {
+    $scope.isDisabled = false;
+    $scope.Second = true;
+    $scope.save = function () {
+      $scope.isDisabled = true;
+      $scope.firts =true;
+      $scope.Second = false;
+    }
 });
